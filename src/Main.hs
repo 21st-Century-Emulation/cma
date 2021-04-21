54dc48bf1dba32bef7d8844187bc8e3835f602f3
David Tyler <david.tyler@metaswitch.com>
2021-04-21 17:16:36 +0100

    Added healthcheck route

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,14 @@ module Main where
 
 import Data.Bits (complement)
 import Cpu (Cpu (..), CpuState (..))
-import Web.Scotty ( json, jsonData, post, scotty )
+import Web.Scotty ( get, json, jsonData, post, scotty, text )
 
 main :: IO ()
 main = do
   scotty 8080 $ do
+    get "/status" $
+      do
+        text "Healthy"
     post "/api/v1/execute" $
       do
         cpu <- jsonData
